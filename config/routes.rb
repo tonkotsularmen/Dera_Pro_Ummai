0fd77bf87e67d4a2b528daa3ca134998167642e4
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-07 01:28:04 +0000

    controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+  end
   devise_for :users, controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
@@ -7,6 +10,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    root to: 'homes#top'
+  end 
+  
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
