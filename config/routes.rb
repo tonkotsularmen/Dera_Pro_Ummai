16e918c25280887b9edf26c3b1e4b3ed08781da2
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-07 13:54:25 +0000

    routing=>namespce,scope module

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,26 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-  end
+
+  #ユーザー
   devise_for :users, controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
 
+  #管理者
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
+  #管理者
   namespace :admin do
     root to: 'homes#top'
-  end 
-  
+  end
 
+
+  #ユーザー
+  scope module: :public do
+    
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
