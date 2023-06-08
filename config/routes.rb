8529ca9978809db48eaa0a27e7344401ab42f095
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-08 06:08:16 +0000

    config/initializers/rack_profile.rb作成

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   #ユーザー
   scope module: :public do
-    
+    root to: 'homes#top'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
