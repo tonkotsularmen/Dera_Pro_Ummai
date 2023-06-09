99318778c96477b04cca08349229dadb2a629aa0
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-09 14:38:12 +0000

    controller modified

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,22 @@
 class Public::UsersController < ApplicationController
   
   def show
+    @user = User.find(params[:id])
   end 
   
   def edit
+    user = User.new(user_params)
+    user.save
+    redirect_to user_path(user.id)
+  end
+  
+  def update
   end 
   
+  
+  private
+  
+    def user_params
+      params.require(:users).permit(:user_name, :email, :goal, :protein, :fat, :carbo)
+    end 
 end
