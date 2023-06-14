8e55e449549fae02fccc770152149c8cfc49c204
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-14 04:23:23 +0000

    status_feed実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,18 @@ class User < ApplicationRecord
       user.password = SecureRandom.urlsafe_base64
     end
   end
-
+  
+  # ユーザーのステータスフィードを返す
+  def feed
+    #Post.where("user_id IN (?) OR user_id = ?",
+    #                      following_ids,        id)
+    #Post.where("user_id IN (:following_ids) OR user_id = :user_id",
+    #                           following_ids: following_ids, user_id: id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+  
 end
 
