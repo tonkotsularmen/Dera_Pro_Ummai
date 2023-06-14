b758772fb99a9a173f0c7880b4fa432ce3024dde
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-14 00:16:25 +0000

    adminゲストログイン削除

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,5 @@
 class Admin::PostsController < ApplicationController
 
-  before_action :ensure_guest_user, only: [:destroy]
 
   def show
     @post = Post.find(params[:id])
@@ -13,12 +12,5 @@ class Admin::PostsController < ApplicationController
     redirect_to admin_user_path(@user)
   end
 
-  private
-
-    def ensure_guest_user
-      if current_user.user_name == "guestuser"
-        redirect_to admin_users_path , notice: 'ゲスト管理者は管理権限を行使できません。'
-      end
-    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
-  devise_scope :admin do
-    post 'admin/guest_sign_in', to: 'admin/users/sessions#guest_sign_in'
-  end
 
   #管理者
   namespace :admin do
