bce1e5a708ac16d40d5b9cb23364a95678ddcf3d
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-16 14:50:47 +0000

    follow一覧作成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,16 +1,18 @@
 class Public::RelationshipsController < ApplicationController
-  
+
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
     user.create_notification_follow!(current_user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    @post = @user.posts
   end
 
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-		redirect_to request.referer
+    @user = User.find(params[:user_id])
+    @post = @user.posts
   end
-  
+
 end
