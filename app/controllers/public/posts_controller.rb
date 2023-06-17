d8a78312f23fce8d62373f14e1675dbef623102b
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-17 02:35:16 +0000

    public/posts/index.html.erbにpaginate追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -16,8 +16,7 @@ class Public::PostsController < ApplicationController
   end
 
   def index
-    #@posts = Post.all.order(created_at: :desc)
-    @posts = current_user.feed.order(created_at: :desc)
+    @posts = Post.page(params[:page]).per(12).order(created_at: :desc)
   end
 
   def show
