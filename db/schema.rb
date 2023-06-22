d931be39b368927e10eb727fa3c1f5903682beed
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-22 10:27:28 +0000

    default削除

--- db/schema.rb
+++ db/schema.rb
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2023_06_20_004041) do
     t.integer "carbo", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "introduction", limit: 100, default: "Introduction"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
