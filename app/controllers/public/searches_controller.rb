f9ede9b0b2316888cce40dd59ae8e132fbfbf9b0
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-24 12:19:14 +0000

    検索機能の修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "User"
+    if @range == "ユーザー"
       @users = User.looks(params[:search], params[:word])
     else
       @posts = Post.looks(params[:search], params[:word])
