27c659f7690a5f132d819b3a7368576fdb0259cd
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-06-27 14:40:57 +0000

    user.rb修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
+      user.user_name = "guestuser"
     end
   end
 
